d352c889a67abf5408cc5382202fc09fdaf364d8
nazar_float <nazarrosytuk@gmail.com>
2024-05-27 10:55:47 +0300

    Это темы для карточек

--- tg_miniapp_ui/lib/src/card_styles.dart
+++ tg_miniapp_ui/lib/src/card_styles.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 
-const CardTheme darkAndroidCardTheme = CardTheme();
-const CardTheme lightIosCardTheme = CardTheme();
-const CardTheme lightAndroidCardTheme = CardTheme();
-const CardTheme darkIosCardTheme = CardTheme();
+final CardTheme darkAndroidCardTheme = CardTheme(
+  clipBehavior: Clip.hardEdge,
+  color: Colors.black,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5.0),
+  ),
+);
+
+final CardTheme lightIosCardTheme = CardTheme(
+  clipBehavior: Clip.hardEdge,
+  color: Colors.white,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5.0),
+  ),
+);
+final CardTheme lightAndroidCardTheme = CardTheme(
+  clipBehavior: Clip.hardEdge,
+  color: Colors.white,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5.0),
+  ),
+);
+final CardTheme darkIosCardTheme = CardTheme(
+  clipBehavior: Clip.hardEdge,
+  color: Colors.black,
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(5.0),
+  ),
+);
