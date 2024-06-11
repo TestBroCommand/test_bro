d5c3cd2ba42434849c1223d39bb605b7c0642e74
hostuser <117681367+hostuserhost@users.noreply.github.com>
2024-06-11 21:39:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:test_bro/src/feature/app/logic/app_runner.dart';
 import 'package:url_strategy/url_strategy.dart';
 
 void main() {
-  setPathUrlStrategy();
+ // setPathUrlStrategy();
   logger.runLogging(
     () => runZonedGuarded(
       () => const AppRunner().initializeAndRun(),
