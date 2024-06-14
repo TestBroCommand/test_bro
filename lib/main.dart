6bb07d0eaf37c1f446586b4c1669c43b306f70f5
hostuser <gwlwexokai@mail.authpass.app>
2024-06-14 21:31:06 +0300

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'dart:async';
 
-import 'package:sentry_flutter/sentry_flutter.dart' as s;
-import 'package:telegram_web_app/telegram_web_app.dart';
 import 'package:test_bro/src/core/utils/logger.dart';
 import 'package:test_bro/src/feature/app/logic/app_runner.dart';
 
 void main() async {
   //setPathUrlStrategy();
-
-  try {
-    Exception(TelegramWebApp.instance.initDataUnsafe!.start_param);
-  } catch (exception, stackTrace) {
-    await s.Sentry.captureException(
-      exception,
-      stackTrace: stackTrace,
-    );
-  }
   logger.runLogging(
     () => runZonedGuarded(
       () => const AppRunner().initializeAndRun(),
--- lib/src/core/utils/router.dart
+++ lib/src/core/utils/router.dart
@@ -1,25 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:telegram_web_app/telegram_web_app.dart';
-import 'package:test_bro/src/core/utils/logger.dart';
 import 'package:test_bro/src/feature/home/widget/home_screen.dart';
 import 'package:test_bro/src/feature/quize/widget/quiz_screen.dart';
 
 final router = GoRouter(
   redirect: (BuildContext context, GoRouterState state) async {
-    try {
-      Exception(TelegramWebApp.instance.initDataUnsafe!.start_param);
-    } catch (exception, stackTrace) {
-      await Sentry.captureException(
-        exception,
-        stackTrace: stackTrace,
-      );
-    }
     if (TelegramWebApp.instance.initDataUnsafe?.start_param != null) {
-      if (TelegramWebApp.instance.initDataUnsafe!.start_param == "quiz") {
-        return null;
-      }
+      return "/quiz/${TelegramWebApp.instance.initDataUnsafe!.start_param}";
     }
     if (state.matchedLocation.contains("/quiz")) {
       return null;
