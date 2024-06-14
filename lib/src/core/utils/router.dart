f1ad32a49c6f99911a6d24d96d3fdd3b17e3459a
hostuser <gwlwexokai@mail.authpass.app>
2024-06-14 16:54:31 +0300

    minor

--- lib/src/core/utils/router.dart
+++ lib/src/core/utils/router.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:telegram_web_app/telegram_web_app.dart';
+import 'package:test_bro/src/core/utils/logger.dart';
 import 'package:test_bro/src/feature/home/widget/home_screen.dart';
 import 'package:test_bro/src/feature/quize/widget/quiz_screen.dart';
 
 final router = GoRouter(
   redirect: (BuildContext context, GoRouterState state) async {
+    if (TelegramWebApp.instance.initDataUnsafe?.start_param != null) {
+      if (TelegramWebApp.instance.initDataUnsafe!.start_param == "quiz") {
+        logger.info(
+            "Start param: ${TelegramWebApp.instance.initDataUnsafe!.start_param}");
+        return null;
+      }
+    }
     if (state.matchedLocation.contains("/quiz")) {
       return null;
     }
