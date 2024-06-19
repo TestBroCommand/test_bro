49903387181157a6762a1c9a97e83f29c5ca763d
hostuser <gwlwexokai@mail.authpass.app>
2024-06-19 20:17:44 +0300

    _

---
+++ tg_miniapp_ui/lib/src/inputdecoration_styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final inputDecorationLightStyle = InputDecorationTheme(
+    border: OutlineInputBorder(), labelStyle: TextStyle(color: Colors.black));
+final inputDecorationDarkStyle =
+    InputDecorationTheme(border: OutlineInputBorder());
